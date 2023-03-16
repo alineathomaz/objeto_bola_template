259a3cc56fa817856dd76343bb951e42be8d0d9d
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-03-15 23:34:04 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+/*crie o objeto ball (bola) e lhe atribua as seguintes propriedades:
+x: 20,
+y: 30,
+r: 30,
+xspeed: 0,
+yspeed: 0,
+color (color será uma matriz com 4 cores à sua escolha) */
+
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  //crie uma bola com a forma circular utilizando a função circle() e os atributos tirados do objeto ball
+
+  //preencha a bola com uma das cores da propriedade color de ball, usando o índice da matriz
+
+  //atribua xspeed como 1
+
+  //faça a bola de mover para a direita com base no incremento de xpeed
+
+}
